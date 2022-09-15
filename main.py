55cf17d72c894b40cfb08b6c1b0b2023ba8c0f9b
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:40:05 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-import time
+import pandas as pd
+
+read_file = pd.read_excel (r'Path where the Excel file is stored\File name.xlsx')
+read_file.to_csv (r'Path to store the CSV file\File name.csv', index = None, header=True)
