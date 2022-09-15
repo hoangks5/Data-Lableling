9d8dbc39a7dd80730120795f281e34a826b0b12d
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:46:05 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import csv
 import numpy
 reader = csv.reader(open("vietnam-rainfall-from-1901-2015-wb.csv", "rb"), delimiter=",")
 x = list(reader)
-result = numpy.array(x).astype("float")
+#result = numpy.array(x).astype("float")
+print(x)
