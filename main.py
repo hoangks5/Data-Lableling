e065ae4ae66ecd54e3bfed9d20a9377d6266ecc1
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 18:10:31 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 
 import pandas as pd
 f=pd.read_csv("vietnam-temperature-from-1901-2015-wb.csv")
-keep_col = ['tas','Year','Month']
+keep_col = ['Month','Year']
 new_f = f[keep_col]
-new_f.to_csv("newFile.csv", index=False)
+print(new_f)
