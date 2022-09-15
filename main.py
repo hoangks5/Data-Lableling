5236f37fa08641cb93e0d69a342e7872c5eab7af
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:40:25 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-read_file = pd.read_excel (r'Path where the Excel file is stored\File name.xlsx')
+read_file = pd.read_excel ('vietnam-rainfall-from-1901-2015-wb.xls')
 read_file.to_csv (r'Path to store the CSV file\File name.csv', index = None, header=True)
