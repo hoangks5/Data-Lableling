bb0eed39378117fb67ae5af601aa4fa1cd4002ca
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:50:35 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 
 import pandas as pd
-f=pd.read_csv("vietnam-rainfall-from-1901-2015-wb.csv")
-keep_col = ['pr','Year','Month']
+f=pd.read_csv("vietnam-temperature-from-1901-2015-wb.csv")
+keep_col = ['tas','Year','Month']
 new_f = f[keep_col]
 new_f.to_csv("newFile.csv", index=False)
