1561ea18b20f3ba3f57aea175f53e90761b15c1d
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:41:55 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
 read_file = pd.read_excel ('vietnam-rainfall-from-1901-2015-wb.xls')
-read_file.to_csv ('e.csv', index = None, header=True)
+read_file.to_csv ('vietnam-rainfall-from-1901-2015-wb.csv', index = None, header=True)
