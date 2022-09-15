0558f4e36abd66cf2800213beec61af40516dacd
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:44:05 +0700

    --- Auto ---

--- main.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-read_file = pd.read_excel ('vietnam-temperature-from-1901-2015-wb.xls')
-read_file.to_csv ('vietnam-temperature-from-1901-2015-wb.csv', index = None, header=True)
