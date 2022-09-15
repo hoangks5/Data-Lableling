2ea99aecdff98b838b593ca5c482f10a0206c3f3
hoangks5 <hoangnd.ute@gmail.com>
2022-09-15 16:47:55 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ reader = csv.reader(open("vietnam-rainfall-from-1901-2015-wb.csv", "rb"), delimi
 x = list(reader)
 #result = numpy.array(x).astype("float")
 print(x)
+
+import pandas as pd
+f=pd.read_csv("vietnam-rainfall-from-1901-2015-wb.csv")
+keep_col = ['day','month','lat','long']
+new_f = f[keep_col]
+new_f.to_csv("newFile.csv", index=False)
