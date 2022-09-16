eb3b85678aed7c3669fed7a447211c367b9ef541
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 10:02:02 +0700

    --- Auto ---

---
+++ main.py
@@ -0,0 +1,16 @@
+
+
+
+import datetime as dt
+
+dates = ['01/02/1991','01/03/1991','01/04/1991']
+x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
+y = range(len(x)) # many thanks to Kyss Tao for setting me straight here
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.plot(x,y)
+plt.gcf().autofmt_xdate()
