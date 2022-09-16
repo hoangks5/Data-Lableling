60e240340b2a0234b12fbaba99e055fee01cfa69
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 11:00:54 +0700

    --- Auto ---

--- showother.py
+++ showother.py
@@ -9,13 +9,13 @@ dates = list(frame['Ngày'])
 import datetime as dt
 
 
-x = [dt.datetime.strptime(d,'%m/%Y').date() for d in dates]
+x = [dt.datetime.strptime(d,'%d/%m/%Y').date() for d in dates]
 y = list(frame['tas'])
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
 plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval = 10000))
 plt.plot(x,y)
 plt.gcf().autofmt_xdate()
