8a8c21edb8e88850adbc6811362f8a1ba5aee708
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 10:11:02 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ dates = list(frame['Month/Year'])
 import datetime as dt
 
 
-x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
+x = [dt.datetime.strptime(d,'%m/%Y').date() for d in dates]
 y = list(frame['pr'])
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator())
 plt.plot(x,y)
 plt.gcf().autofmt_xdate()
