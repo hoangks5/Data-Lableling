41ab17994a2226d1e389e6010419523272c967c8
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 11:00:04 +0700

    --- Auto ---

---
+++ showother.py
@@ -0,0 +1,22 @@
+
+import pandas as pd
+
+frame = pd.read_csv('vietnam-temperature-from-1901-2015-wb.csv')
+
+dates = list(frame['Month/Year'])
+
+
+import datetime as dt
+
+
+x = [dt.datetime.strptime(d,'%m/%Y').date() for d in dates]
+y = list(frame['tas'])
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%Y'))
+plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval = 10000))
+plt.plot(x,y)
+plt.gcf().autofmt_xdate()
+plt.show()
