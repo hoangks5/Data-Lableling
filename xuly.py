7edb2ecb64d0ae0aec709d224dda9a8c27d4c20f
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 11:20:35 +0700

    --- Auto ---

--- xuly.py
+++ xuly.py
@@ -4,9 +4,10 @@
 with open('XAU_USD.csv','r',encoding='utf-8') as f:
     data = f.read().splitlines()
 
+dtt = []
 for i in data[1:]:
     last = float(i.split(',')[2][:-1])
     firt = float(i.split(',')[1][1:])*1000
     gia = str(last + firt)
     dt = i.split(',')[0]+','+gia
-    print(dt)
+    dtt.append(dt)
