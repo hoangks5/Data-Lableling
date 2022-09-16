cd6bdeff333dfea57df5dd1083aea5f13f59b629
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 11:25:35 +0700

    --- Auto ---

--- xuly.py
+++ xuly.py
@@ -1,6 +1,25 @@
 
 
 
+""" with open('Dầu_Thô_WTI.csv','r',encoding='utf-8') as f:
+    data = f.read().splitlines()
+
+dtt = []
+for i in data[1:]:
+    last = float(i.split(',')[2][:-1])
+    firt = float(i.split(',')[1][1:])*1000
+    gia = str(last + firt)
+    dt = i.split(',')[0]+','+gia
+    dtt.append(dt)
+save = '\n'.join(dtt)
+with open('Dầu_Thô_WTI.csv','w',encoding='utf-8') as f:
+    f.write(save)
+    f.close() """
+
+
+
+
+
 with open('Dầu_Thô_WTI.csv','r',encoding='utf-8') as f:
     data = f.read().splitlines()
 
