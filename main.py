a2f597430d57037e583c654ed479d9ef628b874e
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 09:48:41 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -4,8 +4,9 @@
 with open("vietnam-temperature-from-1901-2015-wb.csv",'r',encoding='utf-8') as f:
     data = f.read().splitlines()
 
+t = []
 for i in data:
     nb = i.split(',')[0]
     timest = i.split(',')[2]+'/'+i.split(',')[1]
     string = timest + ',' + nb
-    print(string)
+    
