aaa52535edc0ffb66b3622a749b82a32106858e2
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 10:07:02 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ frame = pd.read_csv('vietnam-rainfall-from-1901-2015-wb.csv')
 dates = list(frame['Month/Year'])
 
 
-""" import datetime as dt
+import datetime as dt
 
-dates = ['01/02/1991','01/03/1991','01/04/1991']
+
 x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
 y = range(len(x)) # many thanks to Kyss Tao for setting me straight here
 
@@ -18,4 +18,4 @@ import matplotlib.dates as mdates
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
 plt.gca().xaxis.set_major_locator(mdates.DayLocator())
 plt.plot(x,y)
-plt.gcf().autofmt_xdate() """
+plt.gcf().autofmt_xdate()
