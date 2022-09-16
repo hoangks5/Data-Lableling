8205a29e6bed222d6194a0ad37ceea604e016c9b
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 09:45:01 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 
 
-import pandas as pd
-f=pd.read_csv("vietnam-temperature-from-1901-2015-wb.csv")
-keep_col = ['Month','Year']
-new_f = f[keep_col]
-print(new_f)
+
+with open("vietnam-temperature-from-1901-2015-wb.csv") as f:
+    
