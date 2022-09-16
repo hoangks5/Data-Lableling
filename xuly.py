0c5a96d751178e1e2a7bce69c73aaeb3337d761a
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 11:11:14 +0700

    --- Auto ---

--- xuly.py
+++ xuly.py
@@ -5,5 +5,5 @@ with open('XAU_USD.csv','r',encoding='utf-8') as f:
     data = f.read().splitlines()
 
 for i in data:
-    last = i.split(',')['1'].replace(',','')
+    last = i.split(',')[1].replace(',','')
     print(last)
