c4672ae4d6e726da46688395da69a3d3830edbc4
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 10:21:22 +0700

    --- Auto ---

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
 plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
-plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval = 10000))
 plt.plot(x,y)
 plt.gcf().autofmt_xdate()
 plt.show()
