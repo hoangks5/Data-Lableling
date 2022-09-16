ab08dbfd2f6d677c02c0ad806133bc2d516b9439
hoangks5 <hoangnd.ute@gmail.com>
2022-09-16 11:27:45 +0700

    --- Auto ---

--- xuly.py
+++ xuly.py
@@ -27,5 +27,8 @@ dtt = []
 for i in data[1:]:
     gia = i.split(',')[1][1:-1]
     dt = i.split(',')[0]+','+gia
-    print(dt)
-    
+    dtt.append(dt)
+save = '\n'.join(dtt)
+with open('Dầu_Thô_WTI.csv','w',encoding='utf-8') as f:
+    f.write(save)
+    f.close()
