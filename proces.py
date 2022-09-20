bdbe279a815458f10eb7d8fd6fc284a10f471085
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 18:13:06 +0700

    --- Auto ---

--- proces.py
+++ proces.py
@@ -6,4 +6,4 @@ import pandas as pd
 path_name = 'BTC_USD.csv' 
 key_cols = ['Date','Price']
 df = pd.read_csv(path_name,usecols=key_cols,skipinitialspace=True)
-df.to_csv(path_name)
+df.to_csv(path_name,index=False)
