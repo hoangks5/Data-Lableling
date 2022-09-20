37fd35e5b8d5d28256b117366761d639a84332b7
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 18:01:06 +0700

    --- Auto ---

--- proces.py
+++ proces.py
@@ -7,19 +7,3 @@ path_name = 'XAU_USD.csv'
 df = pd.read_csv(path_name)
 
 print(df.to_string()) 
-
-with open(path_name,'r',encoding='utf-8') as f:
-    data = f.read().splitlines()
-
-datanew = data['Date']
-
-print(datanew)
-''' dtt = []
-for i in data[1:]:
-    gia = i.split(',')[1]
-    dt = i.split(',')[0][1:-1]+','+gia
-    dtt.append(dt)
-save = '\n'.join(dtt)
-with open(path_name,'w',encoding='utf-8') as f:
-    f.write(save)
-    f.close() '''
