7ae4f580991c35814fa0cc87fc47224e6ffb2a3a
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 18:33:27 +0700

    --- Auto ---

--- del_comma.py
+++ del_comma.py
@@ -11,7 +11,7 @@ for i in data[1:]:
     l = j*1000+k
     dtt.append(i.split(',')[0]+','+str(l))
     
-neww = '/n'.join(dtt)
+neww = '\n'.join(dtt)
 with open('new.csv','w',encoding='utf-8') as f:
     f.write(neww)
     f.close()
