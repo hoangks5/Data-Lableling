0dbdf7f16a169a0696b862222113c9f884c82156
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 18:29:17 +0700

    --- Auto ---

--- del_comma.py
+++ del_comma.py
@@ -4,6 +4,6 @@ with open(path_name,'r',encoding='utf-8') as f:
     data = f.read().splitlines()
     
 for i in data[1:]:
-    j = i.split(',')[1][1:]
+    j = float(i.split(',')[1][1:])
     print(j*1000)
     
