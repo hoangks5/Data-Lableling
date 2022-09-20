5b39cbbbe836138d4da6fc1986a624a78eae3f7d
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 18:31:07 +0700

    --- Auto ---

--- del_comma.py
+++ del_comma.py
@@ -3,8 +3,11 @@ path_name = 'BTC_USD.csv'
 with open(path_name,'r',encoding='utf-8') as f:
     data = f.read().splitlines()
     
+    
+dtt = []
 for i in data[1:]:
     j = float(i.split(',')[1][1:])
     k = float(i.split(',')[2][:-1])
     l = j*1000+k
+    dtt.append(i.split(',')[1][1:]+','+str(l))
     
