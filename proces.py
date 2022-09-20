95de04aca7f2270c8aa502cfbabe6549a9498ee0
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 18:00:56 +0700

    --- Auto ---

--- proces.py
+++ proces.py
@@ -1,24 +1,12 @@
 
+import pandas as pd
 
-
-""" with open('Dầu_Thô_WTI.csv','r',encoding='utf-8') as f:
-    data = f.read().splitlines()
-
-dtt = []
-for i in data[1:]:
-    last = float(i.split(',')[2][:-1])
-    firt = float(i.split(',')[1][1:])*1000
-    gia = str(last + firt)
-    dt = i.split(',')[0]+','+gia
-    dtt.append(dt)
-save = '\n'.join(dtt)
-with open('Dầu_Thô_WTI.csv','w',encoding='utf-8') as f:
-    f.write(save)
-    f.close() """
 
 
 path_name = 'XAU_USD.csv'
+df = pd.read_csv(path_name)
 
+print(df.to_string()) 
 
 with open(path_name,'r',encoding='utf-8') as f:
     data = f.read().splitlines()
