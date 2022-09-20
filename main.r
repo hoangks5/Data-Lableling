52af1a8793f20f90bb747201e59d577995094d41
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 15:25:20 +0700

    --- Auto ---

--- main.r
+++ main.r
@@ -4,3 +4,10 @@ set.seed(1)
 mar_model(seasonal_periods=12) %>%
   generate(length=120, nseries=2) %>%
   autoplot(value)
+
+mar_model(seasonal_periods=c(24, 24*7)) %>%
+  generate(length=24*7*10, nseries=12) %>%
+  autoplot(value)
+
+
+  
