70b891d9c54d3b7850762b72a0e2f174031efa4d
hoangks5 <hoangnd.ute@gmail.com>
2022-09-20 15:07:52 +0700

    --- Auto ---

--- main.r
+++ main.r
@@ -2,3 +2,7 @@ install.packages("gratis")
 devtools::install_github("ykang/gratis")
 library(gratis)
 library(feasts)
+set.seed(1)
+mar_model(seasonal_periods=12) %>%
+  generate(length=120, nseries=2) %>%
+  autoplot(value)
