cd10256b78421989819db9e064f5497a52df4a2a
hoangks5 <hoangnd.ute@gmail.com>
2022-10-03 10:13:25 +0700

    --- Auto ---

--- proces.py
+++ proces.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Lọc cột dữ liệu
 
-path_name = 'Copper Futures Historical Data.csv' 
+path_name = 'Crude Oil WTI Futures Historical Data.csv' 
 key_cols = ['Date','Price']
 df = pd.read_csv(path_name,usecols=key_cols,skipinitialspace=True)
 df.to_csv(path_name,index=False)
