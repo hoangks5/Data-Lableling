8e9385b21ddfcef894f1c66589012ddae5ec3006
hoangks5 <hoangnd.ute@gmail.com>
2022-10-05 15:35:32 +0700

    --- Auto ---

--- proces.py
+++ proces.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Lọc cột dữ liệu
 
-path_name = 'VN Index Historical Data.csv' 
+path_name = 'XAU_USD Historical Data.csv' 
 key_cols = ['Date','Price']
 df = pd.read_csv(path_name,usecols=key_cols,skipinitialspace=True)
 df.to_csv(path_name,index=False)
