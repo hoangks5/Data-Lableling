b1a559391a021a9d9114f859c768f1b09d768684
hoangks5 <hoangnd.ute@gmail.com>
2022-10-05 15:26:11 +0700

    --- Auto ---

--- proces.py
+++ proces.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Lọc cột dữ liệu
 
-path_name = 'AMZN Historical Data.csv' 
+path_name = 'BTC_USD Bitfinex Historical Data.csv' 
 key_cols = ['Date','Price']
 df = pd.read_csv(path_name,usecols=key_cols,skipinitialspace=True)
 df.to_csv(path_name,index=False)
