7d21e760c82471959d2fdd1b28373b8b8615c55a
hoangks5 <hoangnd.ute@gmail.com>
2022-10-05 15:08:30 +0700

    --- Auto ---

--- gachngnang.py
+++ gachngnang.py
@@ -4,7 +4,7 @@ import regex as re
 name_file = 'AAPL Historical Data.csv'
 with open(name_file,'r',encoding='utf-8') as f:
     data = f.read()
-convert = re.sub('/','-',data)
+convert = re.sub('-','/',data)
 print(convert)
 with open(name_file,'w',encoding='utf-8') as f:
     f.write(convert)
