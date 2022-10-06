26f70511f0e5a56adf102f2f56eaf88adab58c38
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 10:12:29 +0700

    --- Auto ---

--- daonguoc.py
+++ daonguoc.py
@@ -1,4 +1,4 @@
-name_file = 'EUR_USD Historical Data.csv'
+name_file = 'Silver Futures Historical Data.csv'
 with open(name_file,'r',encoding='utf=8') as f:
     data = f.read().splitlines()
     data.reverse()
