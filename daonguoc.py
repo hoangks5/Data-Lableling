46c4a01267450c75af19ded9d4b25c30b6bb3873
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 10:11:29 +0700

    --- Auto ---

--- daonguoc.py
+++ daonguoc.py
@@ -1,4 +1,4 @@
-name_file = 'Crude Oil WTI Futures Historical Data.csv'
+name_file = 'ETH_USD Binance Historical Data.csv'
 with open(name_file,'r',encoding='utf=8') as f:
     data = f.read().splitlines()
     data.reverse()
