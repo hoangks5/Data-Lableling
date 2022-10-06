9b98119f0f326e853fe75092375ded9f109bb22f
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 10:10:19 +0700

    --- Auto ---

--- daonguoc.py
+++ daonguoc.py
@@ -1,4 +1,4 @@
-name_file = 'AMZN Historical Data.csv'
+name_file = 'BTC_USD Bitfinex Historical Data.csv'
 with open(name_file,'r',encoding='utf=8') as f:
     data = f.read().splitlines()
     data.reverse()
