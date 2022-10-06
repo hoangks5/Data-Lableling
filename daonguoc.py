5dd554049b14e60c82c3bad80c5481f3a8ebb568
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 09:52:28 +0700

    --- Auto ---

--- daonguoc.py
+++ daonguoc.py
@@ -2,3 +2,6 @@ name_file = ''
 with open(name_file,'r',encoding='utf=8') as f:
     data = f.read().splitlines()
     datanew = data.reverse()
+with open(name_file,'w',encoding='utf=8') as f:
+    f.write(datanew)
+    f.close()
