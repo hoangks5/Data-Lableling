7e9e64124c0571467bb6160513e521a8b1640dd0
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 10:11:59 +0700

    --- Auto ---

--- daonguoc.py
+++ daonguoc.py
@@ -1,4 +1,4 @@
-name_file = 'ETH_USD Binance Historical Data.csv'
+name_file = 'EUR_USD Historical Data.csv'
 with open(name_file,'r',encoding='utf=8') as f:
     data = f.read().splitlines()
     data.reverse()
