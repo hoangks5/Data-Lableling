38a57cfd2e7bff52ec3e0295b48f3105839087f6
hoangks5 <hoangnd.ute@gmail.com>
2022-10-06 09:55:58 +0700

    --- Auto ---

--- daonguoc.py
+++ daonguoc.py
@@ -2,5 +2,14 @@ name_file = 'AAPL Historical Data.csv'
 with open(name_file,'r',encoding='utf=8') as f:
     data = f.read().splitlines()
     datanew = data.reverse()
-    print(datanew)
+    print(data)
     
+    
+    
+    
+    
+    
+'''     datanew1 = '\n'.join(datanew)
+with open(name_file,'w',encoding='utf=8') as f:
+    f.write(datanew1)
+    f.close() '''
