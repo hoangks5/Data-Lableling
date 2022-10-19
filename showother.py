1fae5bb0ed0f446084787f42f60e0484631750e1
hoangks5 <hoangnd.ute@gmail.com>
2022-10-19 10:31:56 +0700

    --- Auto ---

--- showother.py
+++ showother.py
@@ -1,21 +1,21 @@
 
 import pandas as pd
 
-frame = pd.read_csv('AAPL Historical Data.csv')
+frame = pd.read_csv('t.csv')
 
-dates = list(frame['Date'])
+dates = list(frame['DATE'])
 
 
 import datetime as dt
 
 
-x = [dt.datetime.strptime(d,'%m/%d/%Y').date() for d in dates]
+x = [dt.datetime.strptime(d,'%d-%m-%Y').date() for d in dates]
 y = list(frame['Price'])
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%m-%Y'))
 plt.gca().xaxis.set_major_locator(mdates.HourLocator(interval = 5000))
 plt.plot(x,y)
 plt.gcf().autofmt_xdate()
